f698e21e65043cbb7848958d962c8e8702073c83
“prapassorn.uka” <uprapassorn@gmail.com>
2023-05-11 18:35:05 +0700

    Create getUsers method for endpoint /v1/users/{seed}

--- src/main/kotlin/com/sample/user/controller/UserController.kt
+++ src/main/kotlin/com/sample/user/controller/UserController.kt
@@ -1,4 +1,25 @@
 package com.sample.user.controller
 
-class UserController {
+import com.sample.user.model.UserResponse
+import com.sample.user.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/users")
+class UserController(var userService: UserService) {
+
+    @GetMapping("/{seed}")
+    fun getUsers(@PathVariable("seed") seed : String) : ResponseEntity<List<UserResponse>> {
+        var users : List<UserResponse>? = userService.findUsers(seed);
+        return if (users != null) {
+            ResponseEntity(users, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+    }
 }
